0098004384218c2a9eb898a4f5b00c88f30a9278
stefanpython <stefanhtml@yahoo.com>
2022-11-15 18:52:46 +0200

    Minor image path changes

--- src/meniu.js
+++ src/meniu.js
@@ -13,25 +13,25 @@ export function mainMeniu() {
     <h3 id="title">Burgers</h3>
     
     <h4>Blue Cat Cheeseburger*</h4>
-    <img class="menuImg" src="/dist/images/burger2.jpg" alt="cat">
+    <img class="menuImg" src="images/burger2.jpg" alt="cat">
     <p>blue cheese, caramelized onions, crispy onion ring, frisée, blue cheese dressing, bibb lettuce, spicy aioli</p>
     <p id="price" style="margin-left: 300px;">$17.00</p>
     <br><hr>
     
     <h4>Vegan Meow Burger</h4>
-    <img class="menuImg" src="/dist/images/vegan.jpg" alt="cat">
+    <img class="menuImg" src="images/vegan.jpg" alt="cat">
     <p>plant-based patty, vegan bun, bibb lettuce, roasted tomato, avocado, eggless garlic mayo</p>
     <p id="price" style="margin-left: 300px;">$19.00</p>
     <br><hr>
 
     <h4>Backyard Cat Burger</h4>
-    <img class="menuImg" src="/dist/images/vegan.jpg" alt="cat">
+    <img class="menuImg" src="images/vegan.jpg" alt="cat">
     <p>american cheese, bibb lettuce, tomato, red onion, house pickles</p>
     <p id="price" style="margin-left: 300px;">$21.00</p>
     <br><hr>
 
     <h4>Forest Cat Burger</h4>
-    <img class="menuImg" src="/dist/images/burger3.jpg" alt="cat">
+    <img class="menuImg" src="images/burger3.jpg" alt="cat">
     <p>swiss, mixed mushrooms, fried egg, arugula, porcini aioli</p>
     <p id="price" style="margin-left: 300px;">$16.00</p>
     `;
@@ -48,19 +48,19 @@ export function mainMeniu() {
     <h3 id="title">Drinks</h3>
 
     <h4>STICKY CAT SHAKE</h4>
-    <img class="menuImg" src="/dist/images/drink1.jpg" alt="">
+    <img class="menuImg" src="images/drink1.jpg" alt="">
     <p>brown butter milkshake, toffee sauce, whipped cream</p>
     <p id="price" style="margin-left: 300px;">$9.00</p>
     <br><hr>
 
     <h4>Snowball Coffee Cat</h4>
-    <img class="menuImg" src="/dist/images/drink2.jpg" alt="">
+    <img class="menuImg" src="images/drink2.jpg" alt="">
     <p>roasted mayan signiture caffee beans, milk, fijy water, honney</p>
     <p id="price" style="margin-left: 300px;">$11.00</p>
     <br><hr>
 
     <h4>Drunken Meowshi Beer</h4>
-    <img class="menuImg" src="/dist/images/drink3.jpg" alt="">
+    <img class="menuImg" src="images/drink3.jpg" alt="">
     <p>weat, barley, grain, hops, yeast, special secret ingredient - it's water</p>
     <p id="price" style="margin-left: 300px;">$7.00</p>`;
 
@@ -76,19 +76,19 @@ export function mainMeniu() {
     <h3 id="title">Fish</h3>
 
     <h4>SwordFish katsu</h4>
-    <img class="menuImg" src="/dist/images/fish1.jpg" alt="">
+    <img class="menuImg" src="images/fish1.jpg" alt="">
     <p>soy glaze, shaved brussels sprout, xo vinaigrette, jasmine rice</p>
     <p id="price" style="margin-left: 300px;">$19.00</p>
     <br><hr>
 
     <h4>Catfish Meowunchy</h4>
-    <img class="menuImg" src="/dist/images/fish2.jpg" alt="">
+    <img class="menuImg" src="images/fish2.jpg" alt="">
     <p>sweet potato, salsa macha, jicacma, peanut</p>
     <p id="price" style="margin-left: 300px;">$24.00</p>
     <br><hr>
 
     <h4>Pawsfish Catigaro</h4>
-    <img class="menuImg" src="/dist/images/fish3.jpg" alt="">
+    <img class="menuImg" src="images/fish3.jpg" alt="">
     <p>kimchi stew, tofu, enoki mushroom, housemade "spam"</p>
     <p id="price" style="margin-left: 300px;">$17.00</p>`;
 
